f91c247418a6882110ad3a944fc4885dc368aab0
Igor <steamakkigor@mail.ru>
2024-08-16 21:19:38 +0500

    Fixed Docker

--- app/bot/middlewares/__init__.py
+++ app/bot/middlewares/__init__.py
@@ -1,3 +1,3 @@
 from aiogram.utils.i18n import FSMI18nMiddleware, I18n
 
-i18n = FSMI18nMiddleware(I18n(path="locales", default_locale="ru", domain="messages"))
+i18n = FSMI18nMiddleware(I18n(path="app/locales", default_locale="ru", domain="messages"))
